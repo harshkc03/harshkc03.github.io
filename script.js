10a3781890bf04b8b4e4bf6e410962d39449f272
Harsh Chaudhary <harshkc2000@gmail.com>
2023-07-15 15:20:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var form = document.getElementById('mqttForm');
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var imei = document.getElementById('imei').value;
+        var amount = document.getElementById('amount').value;
+        var language = document.getElementById('language').value;
+
+        sendMessage(imei, amount, language);
+    });
+
+    function sendMessage(imei, amount, language) {
+        var topic = '/kiotel/sb/' + imei;
+        var message = amount + '#' + language + '#';
+
+        var client = new Paho.MQTT.Client('broker.emqx.io', 8083, 'clientId');
+
+        client.onConnectionLost = function (responseObject) {
+            console.log('Connection lost: ' + responseObject.errorMessage);
+        };
+
+        client.connect({
+            onSuccess: function () {
+                console.log('Connected to the broker');
+                var messagePayload = new Paho.MQTT.Message(message);
+                messagePayload.destinationName = topic;
+                client.send(messagePayload);
+                console.log('Message sent');
+                // client.disconnect();
+            },
+            onFailure: function (message) {
+                console.log('Connection failed: ' + message.errorMessage);
+            }
+        });
+    }
+});
